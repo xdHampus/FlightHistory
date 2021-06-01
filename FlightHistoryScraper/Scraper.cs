c362ff730bdd7bd9bb6013321e38a525471ff346
Hampus <16954508+xdHampus@users.noreply.github.com>
2021-06-01 03:15:56 +0200

    Rework

---
+++ FlightHistoryScraper/Scraper.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightHistoryCore;
+using FlightHistoryCore.Model;
+
+namespace FlightHistoryScraper
+{
+    public class Scraper
+    {
+        private const string radarAreaAPIUrl = "https://data-live.flightradar24.com/zones/fcgi/feed.js?faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1";
+        private const string flightUrl = "https://data-live.flightradar24.com/clickhandler/?version=1.5";
+        private const string bounds = "78.618%2C39.928%2C-121.739%2C-37.101";
+
+        private string radarAreaUrl = $"{radarAreaAPIUrl}&bounds={bounds}";
+        private ScanQueue scanTargets = new();
+
+
+        private WebRequester webRequester = WebRequester.Instance;
+
+
+        public Scraper()
+        {
+            using var db = new FlightDbContext();
+            db.Database.EnsureCreated();
+        }
+
+        public void StartScraper()
+        {
+            ScanArea();
+            ScanQueuedItems();
+
+            StartScraper();
+        }
+
+        private void ScanArea()
+        {
+            Console.WriteLine($"Starting area scan at {CurrentDateTime()}.");
+            string areaData = GetAreaData();
+            if (!string.IsNullOrEmpty(areaData))
+            {
+                List<string> flightIds = GetFlightIds(areaData);
+                for (int i = 0; i < flightIds.Count; i++)
+                {
+                    Console.WriteLine($"Area scanning flight {flightIds[i]} at {CurrentDateTime()} - {i} out of {flightIds.Count}.");
+                    AreaScanFlight(flightIds[i]);
+                }
+            }
+            Console.WriteLine($"Ending area scan at {CurrentDateTime()}.");
+        }
+
+        private void AreaScanFlight(string fId)
+        {
+            if (!scanTargets.FlightExists(fId))
+            {
+                if (!FlightInDB(fId))
+                {
+                    Console.WriteLine("Flight is new.");
+                    ScanNewFlight(fId);
+                }
+                else if (FlightScanIncomplete(fId))
+                {
+                    Console.WriteLine("Flight was not scanned properly, queuing again.");
+                    scanTargets.Enqueue(GetJsonFlight(fId).Convert());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Flight already scanned.");
+            }
+        }
+
+
+        private bool FlightScanIncomplete(string id)
+        {
+            using var db = new FlightDbContext();
+            Flight flight = db.Flights
+                .Where(f => f.Identification.Id == db.FlightIdentifications
+                    .Where(fId => fId.FlightIdentifier.Equals(id))
+                    .FirstOrDefault().Id)
+                .FirstOrDefault();
+                
+            return !flight.ScanCompleted.GetValueOrDefault();
+        }
+
+        private bool FlightInDB(string id)
+        {
+
+            using var db = new FlightDbContext();
+            return db.FlightIdentifications
+                    .Any(fId => fId.FlightIdentifier.Equals(id));
+        }
+
+        private void ScanNewFlight(string id)
+        {
+            Json.Flight jsonFlight = GetJsonFlight(id);
+            if(jsonFlight != null)
+            {
+                using var db = new FlightDbContext();
+                Flight flight = jsonFlight.Convert();
+
+                if(flight != null && flight.Identification != null)
+                {
+                    db.Flights.Add(flight);
+                    db.SaveChanges();
+                    scanTargets.Enqueue(flight);
+                } 
+                else
+                {
+                    Console.WriteLine("Error converting new flight.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error scanning new flight.");
+            }
+        }
+
+        private void RescanFlight(Flight flight)
+        {
+            using var db = new FlightDbContext();
+            var res = GetJsonFlight(flight.Identification.FlightIdentifier);
+
+
+            if (res == null || res.Trail != null && res.Trail.Count > flight.Trails.Count)
+            {
+                Console.WriteLine($"Found {res.Trail.Count - flight.Trails.Count} new trails");
+                flight.Trails.AddRange(res.Trail
+                    .Skip(flight.Trails.Count)
+                    .ToList()
+                    .ConvertAll(t => t.Convert()));
+            }
+            else
+            {
+                Console.WriteLine("Scan completed.");
+                flight.ScanCompleted = true;
+            }
+
+
+            db.SaveChanges();
+            if (!flight.ScanCompleted.Value)
+            {
+                scanTargets.Enqueue(flight);
+            }
+        }
+
+        private void ScanQueuedItems()
+        {
+            int initSize = scanTargets.Count;
+            for (int i = 0; i < initSize; i++)
+            {
+                Flight flight = scanTargets.Dequeue();
+                Console.WriteLine($"Recanning flights {flight.Identification.FlightIdentifier} at {CurrentDateTime()} - {i} out of {initSize}.");
+                RescanFlight(flight);
+            }
+        }
+
+
+
+
+        private Json.Flight GetJsonFlight(string id)
+        {
+            var data = GetFlightData(id);
+            var result = Json.Flight.FromJson(data);
+
+            if (result == null || result.Trail == null)
+            {
+                Console.WriteLine($"Error getting data from flight {id}; pasting data below");
+                Console.WriteLine(data);
+            }
+
+            return result;
+        }
+
+        private List<string> GetFlightIds(string jsonRadarArea)
+        {
+            var result = new List<string>();
+            foreach (var item in Json.RadarArea.FromJson(jsonRadarArea).ToList())
+            {
+                if (item.Value.AnythingArray != null && item.Value.AnythingArray.Count == 19) //why is this 19?
+                {
+                    result.Add(item.Key);
+                }
+            }
+            return result;
+        }
+
+
+        private string GetFlightData(string id)
+        {
+            return webRequester.GetHTMLAsync($"{flightUrl}&flight={id}").Result;
+        }
+
+        private string GetAreaData()
+        {
+            return webRequester.GetHTMLAsync(radarAreaUrl).Result;
+        }
+
+        private string CurrentDateTime()
+        {
+            return DateTime.Now.ToString("dd MMMM yyy HH:mm:ss");
+        }
+
+    }
+}
--- FlightHistoryScraper/WebRequester.cs
+++ FlightHistoryScraper/WebRequester.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -10,141 +11,98 @@ namespace FlightHistoryScraper
   
     public sealed class WebRequester
     {
-    
+
+        private const int baseRateDelay = 250;
+        private const int rateDelayIncrementer = 2;
+        private const int maxDelayIncrements = 16;
+
+        private HttpClient client = new();
+        private DateTime lastQuery = DateTime.Now;
+        private int currentDelayIncrement = rateDelayIncrementer;
+
+
         private static readonly Lazy<WebRequester>
             lazy =
             new Lazy<WebRequester>
                 (() => new WebRequester());
 
-        private const int rateDelay = 1000;
-        private const int rateDelayMultiplier = 3;
-		private const int maxDelayMultiplier = 18;
 
-        
-        private const string radarAreaUrl = "https://data-live.flightradar24.com/zones/fcgi/feed.js?faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1";
-        private const string flightUrl = "https://data-live.flightradar24.com/clickhandler/?version=1.5";
-		private const string dateTimeFormat = "dd MMMM yyy HH:mm:ss";
-		private HttpClient client = new HttpClient();
 
         public static WebRequester Instance { get { return lazy.Value; } }
-        private int currentRateDelayM = 1;
-		
-
+	
         private WebRequester()
         {
         }
 
-        private DateTime lastQuery = DateTime.Now;
+
+        public async Task<string> GetHTMLAsync(string url)
+        {
+            string result = string.Empty;
+            await RateLimit();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+				if(response.IsSuccessStatusCode){
+					result = await response.Content.ReadAsStringAsync();
+                    resetDelay();
+                }
+                else
+                {
+                    increaseDelay();
+                    Console.WriteLine($"Unable to get HTML, setting delay to {currentDelayMs()}ms."); ;
+                    return GetHTMLAsync(url).Result;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            lastQuery = DateTime.Now;
+            return result;
+        }
+
+
+
+
 
         private async Task RateLimit()
         {
-        
-			int targetDelay = currentDelay();
-            TimeSpan span = DateTime.Now - lastQuery;
-            int ms = (int)span.TotalMilliseconds;
 
-            Console.WriteLine($"	rate limit to {ms}ms");
+            int ms = (int)(DateTime.Now - lastQuery).TotalMilliseconds;
+            int currentDelay = currentDelayMs();
 
-            lastQuery = DateTime.Now;
-
-            if (ms > 0 && ms < targetDelay)
+            if (ms < currentDelay && ms > 0)
             {
-                int timeToWait = targetDelay - ms;
-                if(timeToWait > targetDelay) {
-                  timeToWait = targetDelay;
-                }
-                
-				Console.WriteLine($"	actually limiting to {timeToWait}ms");              
-                await Task.Delay(timeToWait);
-
-                if(currentRateDelayM > 1){
-                	currentRateDelayM /= rateDelayMultiplier;
-                }
+                await Task.Delay(currentDelay - ms);
             }
-
         }
 
-		private int currentDelay(){
-			return rateDelay * rateDelayMultiplier * currentRateDelayM;
-		}
-		private void increaseMultiplier(){
-			if(currentRateDelayM < maxDelayMultiplier){
-				currentRateDelayM *= rateDelayMultiplier;
-			}
-		}        
 
-        public async Task<string> GetRadarArea(string bounds = "78.618%2C39.928%2C-121.739%2C-37.101")
+        private int currentDelayMs()
         {
-            string result = string.Empty;
-
-            await RateLimit();
-
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync($"{radarAreaUrl}&bounds={bounds}");
-                response.EnsureSuccessStatusCode();
-
-				if(response.IsSuccessStatusCode){
-					result = await response.Content.ReadAsStringAsync();
-                	Console.WriteLine($"	read areadata {DateTime.Now.ToString(dateTimeFormat)}");
-					currentRateDelayM = 1;
-				}
-				else if(response.StatusCode == System.Net.HttpStatusCode.TooManyRequests) {
-					Console.WriteLine($"	too many requests {DateTime.Now.ToString(dateTimeFormat)}");
-					currentRateDelayM *= rateDelayMultiplier;
-					result = await GetRadarArea(bounds);
-				} else {
-					Console.WriteLine($"	error {response.StatusCode} at {DateTime.Now.ToString(dateTimeFormat)}");
-				}
-			
-
-                
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-
-            return result;
+            return baseRateDelay * currentDelayIncrement;
         }
 
-        public async Task<string> GetFlightData(string id)
+        private void increaseDelay()
         {
-            string result = string.Empty;
-
-			Console.WriteLine($"	starting await for {id} at {DateTime.Now.ToString(dateTimeFormat)}");
-            await RateLimit();
-			Console.WriteLine($"	await finished at {DateTime.Now.ToString(dateTimeFormat)}");
-
-            try
+            if (rateDelayIncrementer + currentDelayIncrement < maxDelayIncrements)
             {
-                HttpResponseMessage response = await client.GetAsync($"{flightUrl}&flight={id}");
-                
-				if(response.IsSuccessStatusCode){
-					result = await response.Content.ReadAsStringAsync();
-                	Console.WriteLine($"	read areadata {DateTime.Now.ToString(dateTimeFormat)}");
-                	currentRateDelayM = 1;
-				}
-				else if(response.StatusCode == System.Net.HttpStatusCode.TooManyRequests) {
-					Console.WriteLine($"	too many requests {DateTime.Now.ToString(dateTimeFormat)}");
-					currentRateDelayM *= rateDelayMultiplier;
-					result = await GetFlightData(id);
-				} else {
-					Console.WriteLine($"	error {response.StatusCode} at {DateTime.Now.ToString(dateTimeFormat)}");
-				}
+                currentDelayIncrement += rateDelayIncrementer;
             }
-            catch (Exception e)
+        }
+        private void decreaseDelay()
+        {
+            if (currentDelayIncrement - rateDelayIncrementer > rateDelayIncrementer)
             {
-                Console.WriteLine(e.Message);
+                currentDelayIncrement -= rateDelayIncrementer;
             }
-
-		    Console.WriteLine($"	Returning at {DateTime.Now.ToString(dateTimeFormat)}");
-            return result;
-
-
+        }
+        private void resetDelay()
+        {
+            currentDelayIncrement = rateDelayIncrementer;
         }
 
     }
-
 }
